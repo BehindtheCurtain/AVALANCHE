c7cebb1d6e554aacf0510ac3e81fb1967f8d8f04
Austen Herbst <austenherbst@yahoo.com>
2013-04-16 16:09:11 -0500

    Added Working Real-Time Gauges

--- ECSlidingViewController/ScanController.m
+++ ECSlidingViewController/ScanController.m
@@ -92,7 +92,7 @@
     [AppDelegate app].activePeripheral = [_peripherals objectAtIndex:indexPath.row];
     [self.deviceTableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    ConnectionController* connection = [self.storyboard instantiateViewControllerWithIdentifier:@"Real Time View"];
+    ConnectionController* connection = [self.storyboard instantiateViewControllerWithIdentifier:@"View1"];
     
     [self.navigationController pushViewController:connection animated:YES];
     
